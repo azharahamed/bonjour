5477eb506a463dbc3bdf16627fc0fe3e3fb20619
azharahamed <azharahamed@users.noreply.github.com>
2018-07-22 23:03:47 -0400

    bonjur intial code downloaded from the spring.io initializer

---
+++ src/main/java/com/bonjour/bonjour/BonjourApplication.java
@@ -0,0 +1,12 @@
+package com.bonjour.bonjour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonjourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonjourApplication.class, args);
+	}
+}
